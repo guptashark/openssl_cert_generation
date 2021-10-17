d56d8fa2a8a916b3d9ea7e64b9d8977f8898e437
Aishwary Gupta <guptashark@protonmail.com>
2021-10-17 10:25:37 -0400

    Use ERR_get_error fns to use ssl's error stack
    OpenSSL has an error stack that you can query
    when an operation fails. It's hard to put meaning
    to the error codes, so SSL provides a conversion
    fn so you can get a human readable string.

--- client.c
+++ client.c
@@ -7,10 +7,18 @@
 int main(void) {
  
   int ret = 0;
+  unsigned long err = 0;
+  char err_buff[256] = {'\0'};
 
   SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
-  ret = SSL_CTX_load_verify_locations(ctx, "ca_01.crt", NULL);
-  if (ret == 0) printf("Could not load ca file.\n");
+  ret = SSL_CTX_load_verify_locations(ctx, "ca_bad_name.crt", NULL);
+  if (ret == 0) {
+    do {
+      err = ERR_get_error();
+      ERR_error_string(err, err_buff);
+      printf("%s\n", err_buff);
+    } while (err != 0);
+  }
 
   BIO *bio = BIO_new(BIO_s_connect());
   BIO_set_conn_hostname(bio, "127.0.0.1");
